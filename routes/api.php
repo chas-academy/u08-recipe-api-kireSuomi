43a7579ca768418055ee8b1185bb1755ef624951
Erik <kireSuomi@gmail.com>
2021-12-10 16:30:16 +0100

    Added api routes, user and reciepe-list operations (protected)

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@
 use App\Http\Controllers\RecipelistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 
 
 /*
@@ -18,14 +18,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/recipelist', [RecipelistController::class, 'index']);
 
 Route::get('/', function()  {
     return 'API working';
 });
 
-Route::get('/recipelist', [RecipelistController::class, 'index']);
-Route::post('/recipelist', [RecipelistController::class, 'store']);
-Route::get('/recipelist/{id}', [RecipelistController::class, 'show']);
+
+// ---------- User operations ----------
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function ()  {
+
+    // ---------- Recipelist operations ----------
+
+        //Return all recipelists, this is test only.
+        Route::get('/recipelist', [RecipelistController::class, 'index']);
+
+        //Create a new recipelist
+        Route::post('/recipelist', [RecipelistController::class, 'store']);
+
+        //Get a single recipelist
+        Route::get('/recipelist/{id}', [RecipelistController::class, 'show']);
+
+        //Add a recipe to a list
+        Route::put('/recipelist/add/{id}', [RecipelistController::class, 'update_add']);
+
+        //remove a recipe to a list
+        Route::put('/recipelist/remove/{id}', [RecipelistController::class, 'update_remove']);
+
+        //Remove recipe list
+        //remove a recipe to a list
+        Route::delete('/recipelist/{id}', [RecipelistController::class, 'destroy']);
+});
